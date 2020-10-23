15b0bb63cc269fdbec33bae8573fec3ba8b4cd88
ofadiman <ofadiman@gmail.com>
2020-10-23 11:41:22 +0200

    Making changes

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Making some random changes to see if it works.
         </p>
         <a
           className="App-link"
